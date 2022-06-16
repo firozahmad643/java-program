3f55aa425a4deabac9a23bfb18250920276e7b33
firozahmad643 <107192510+firozahmad643@users.noreply.github.com>
2022-06-16 12:22:50 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,145 @@
+/*
+*****
+*****
+*****
+*****
+*****
+*****
+*/
+
+/*public class  pattern {
+
+	public static void main(String[] args)
+	
+	{ 
+		 int i,j;
+		 for(i=1;i<=5;i++) 
+                               {
+		
+		 
+	    		 
+	               for(j=1;j<=5;j++)
+	             {
+	    	
+	    	System.out.print("*");
+	             }
+
+                                 System.out.println();
+
+	           }
+	}
+
+}*/
+
+
+/*
+*
+**
+***
+****
+*****
+*/
+
+
+/*
+public class  pattern {
+
+	public static void main(String[] args)
+	
+	{ 
+		 int i,j;
+		 for(i=1;i<=5;i++) 
+                               {
+		
+		 
+	    		 
+	               for(j=1;j<=i;j++)
+	             {
+	    	
+	    	System.out.print("*");
+	             }
+
+                                 System.out.println();
+
+	           }
+	}
+
+}*/
+
+
+/*
+*****
+****
+***
+**
+*
+*/
+
+
+/*public class  pattern {
+
+	public static void main(String[] args)
+	
+	{ 
+		 int i,j;
+		 for(i=1;i<=5;i++) 
+                               {
+		
+		 
+	    		 
+	               for(j=i;j<=5;j++)
+	             {
+	    	
+	    	System.out.print("*");
+	             }
+
+                                 System.out.println();
+
+	           }
+	}
+
+}*/
+
+
+/*
+1
+10
+101
+1010
+10101
+*/
+
+
+/*
+public class  pattern {
+
+	public static void main(String[] args)
+	
+	{ 
+		 int i,j;
+		 for(i=1;i<=5;i++) 
+                               {
+		
+		 
+	    		 
+	               for(j=1;j<=i;j++)
+	             {
+	    	if(j%2==0 )
+                                          {
+	    	System.out.print("0");
+                                         }
+                                        else
+                                           {
+                                              System.out.print("1");
+                                            }
+                                            
+	                 } 
+                                        System.out.println();
+
+                                
+
+	         }
+	}
+
+}*/
+
