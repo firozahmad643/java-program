cf4fa9e650db7fc60a2146be50c91d59ab1d83e5
firozahmad643 <107192510+firozahmad643@users.noreply.github.com>
2022-06-16 12:17:30 +0530

    Add files via upload

--- operator_2.java
+++ operator_2.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 public  class operator_2{
 
-                                    public static void main (string[] args)
+                                    public static void main (String[] args)
                                     {
 
                                            arithmetic_opt();
-
+                                           System.out.println();  
+                                           assignment_opt();
+                                           System.out.println();  
+                                           logical_opt();
 
 
                                       }
@@ -17,36 +20,120 @@ public  class operator_2{
 
                                         Scanner sc = new Scanner(System.in);
                   
-                                       static  int a,b,add,,mul,sub,div,mod;     
+                                         int a,b,add,mul,sub,div,mod; 
+                                         System.out.println("\n arithmetic function \n");     
                         
-                                     System.out.println("Enter Two Numbers:");  
+                                       System.out.println("Enter Two Numbers:");  
            
-                                   a=sc.nextInt();                                         
+                                      a=sc.nextInt();                                         
                                       b=sc.nextInt();
 
                                         add=a+b;  
                                                
                                         mul=a*b;
              
-                                       sub=a-b;
+                                        sub=a-b;
 
-                                    div=a/b;
+                                       div=a/b;
 
-                                     mod=a%b;
+                                      mod=a%b;
   
-                                 System.out.println("Addition Of Two Numbers:"+add);      
+                                   System.out.println("Addition Of Two Numbers:"+add);      
 
-                                 System.out.println("Multiplication Of Two Numbers:"+mul);
+                                  System.out.println("Multiplication Of Two Numbers:"+mul);
 
-                             System.out.println("Subtraction Of Two Numbers:"+sub);
+                                  System.out.println("Subtraction Of Two Numbers:"+sub);
  
                                  System.out.println("Division Of Two Numbers:"+div);     
 
-                               System.out.println("Modulus Of Two Numbers:"+mod);       
+                                  System.out.println("Modulus Of Two Numbers:"+mod);       
 
 
+                           }
+               
+                   
+                             public static void assignment_opt()
+                      
+                         {
+                               Scanner sc=new Scanner(System.in);                                
+                               int a,b,c,d,e,f;
+                                System.out.println("\n assignment function \n"); 
+                               System.out.println("enter six number");               
+                              a= sc.nextInt();                                                  
+                              b= sc.nextInt();
+                              c= sc.nextInt();
+                              d= sc.nextInt();
+                              e= sc.nextInt();
+                              f= sc.nextInt();
+
+                                a+=a;                                                              
+                               c-=a;
+                               d*=a;
+                              e/=a;
+                                f%=a;
+
+                                 System.out.println("Addition Assignment Operator Result:"+a);      
+                                  System.out.println("Subtraction Assignment Operator Result:"+c);   
+                                   System.out.println("Multiplication Assignment Operator Result:"+d);
+                                  System.out.println("Division Assignment Operator Result:"+e);      
+                                 System.out.println("Modulus Assignment Operator Result:"+f);  
+
+                
+                           }
+
+                              
+                                     public static void logical_opt()
+                      
+                         {
+                              Scanner sc = new Scanner(System.in);      
+
+                                 int a,b,c,d; 
+      
+                             System.out.println("\n logical function \n"); 
+                              System.out.println("Enter Four Numbers:");
+
+                              a=sc.nextInt();                           
+
+                             b=sc.nextInt();
+
+                            c=sc.nextInt();
+
+                           d=sc.nextInt();
+
+                           System.out.println((a>b)&&(c<d));      
+   
+                          System.out.println((a<b)||(c>=d));      
+   
+                           System.out.println(!(c>a));  
+
+                
                            }
 
 
 
-                   }
+
+
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
